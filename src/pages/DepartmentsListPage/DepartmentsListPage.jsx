270ee18cd55e8b3028fdf33d4bf81914e4662386
Kateryna <k.borysenko.kyiv@gmail.com>
2023-12-01 12:11:52 +0200

    added DepartmentPage and DepartmentsListPage

--- src/pages/DepartmentsListPage/DepartmentsListPage.jsx
+++ src/pages/DepartmentsListPage/DepartmentsListPage.jsx
@@ -1,10 +1,49 @@
-import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Paper from "../../components/common/Paper/Paper";
+import Header from "../../components/Header/Header";
+import * as api from "../../services/api";
 import s from "./DepartmentsListPage.module.css";
 
-const DepartmentsListPage = (props) => {
-  return <div>DepartmentsListPage</div>;
-};
+const API_ENDPOINT = "departments";
+const DepartmentsListPage = () => {
+  const [departments, setDepartments] = useState([]);
+  const location = useLocation();
+  // console.log(location);
 
-DepartmentsListPage.propTypes = {};
+  useEffect(() => {
+    const fetchDepartments = () => {
+      api
+        .getData(API_ENDPOINT)
+        .then(setDepartments)
+        .catch((err) => console.log(err.message));
+    };
+    fetchDepartments();
+  }, []);
+  return (
+    <>
+      <Header title="Факультеты" />
+      {!!departments.length && (
+        <ul>
+          {departments.map(({ id, name }) => (
+            <li key={id} className={s.listElem}>
+              <Link
+                to={id}
+                state={{
+                  from: location,
+                  label: "Назад ко всем факультетам",
+                }}
+              >
+                <Paper>
+                  <p className={s.text}>{name}</p>
+                </Paper>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default DepartmentsListPage;
