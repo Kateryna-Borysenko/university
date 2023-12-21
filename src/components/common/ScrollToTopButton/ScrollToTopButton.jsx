83127650f33f0f2a4bf36b8a5fd27fc9b1f77c70
Kateryna <k.borysenko.kyiv@gmail.com>
2023-12-21 13:50:02 +0200

    added scrollToTopBtn

---
+++ src/components/common/ScrollToTopButton/ScrollToTopButton.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+import s from './ScrollToTopButton.module.css';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className={`${s.scrollBtn} ${isVisible ? s.show : s.hide}`}>
+      <div className={s.button} onClick={scrollToTop}>
+        <FaArrowUp size={16} />
+      </div>
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
